03c881d026231e082a22c26b0545abb01a7518c1
EfrenLG <efrenlozanoguerrero@gmail.com>
2025-06-03 22:20:35 +0200

    chat

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,5 +1,5 @@
 const clients = new Set();
-
+const Message = require('../models/Message'); 
 const chatService = {
     addClient(client) {
         clients.add(client);
@@ -9,11 +9,27 @@ const chatService = {
         clients.delete(client);
     },
 
-    broadcastMessage(messageData) {
-        clients.forEach((client) => {
-            client.send(JSON.stringify(messageData));
-        });
+    async broadcastMessage(messageData) {
+        try {
+            const newMessage = new Message({
+                username: messageData.username,
+                message: messageData.message,
+                postId: messageData.postId
+            });
+
+            await newMessage.save();
+
+            const messageString = JSON.stringify(messageData);
+            clients.forEach((client) => {
+                if (client.readyState === 1) { 
+                    client.send(messageString);
+                }
+            });
+
+        } catch (error) {
+            console.error('Error guardando mensaje:', error);
+        }
     }
 };
 
-module.exports = chatService; 
+module.exports = chatService;
