462717c8cdfaf18cf7ef30291750ecbfccac296b
EfrenLG <efrenlozanoguerrero@gmail.com>
2025-06-04 18:08:56 +0200

    cloudinay

--- src/config/cloudinaryConfig.js
+++ src/config/cloudinaryConfig.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: 'cloudinary://512682476614917:C2c1I0Um8Z7rBuALZ0J7iSC3Qac@dpf3ly5ux',
+  cloud_name: 'dpf3ly5ux',
   api_key: '512682476614917',
   api_secret: 'TC2c1I0Um8Z7rBuALZ0J7iSC3Qac',
 });
